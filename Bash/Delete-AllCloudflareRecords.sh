a3f80618bfa850a00902754cf165b07cb09481c2
Bryan Jones <unix.chkpwd@gmail.com>
2023-10-20 15:53:45 -0400

    dynamically set a key using cli

--- Bash/Delete-AllCloudflareRecords.sh
+++ Bash/Delete-AllCloudflareRecords.sh
@@ -7,7 +7,7 @@
 #     -H "X-Auth-Key: ${KEY}" \
 # for old API keys
 
-TOKEN="l6iKrybTqjQW8t1Fh_XUpqwDs5d3vcRDXoaSpFAF"
+TOKEN="${CLOUDFLARE_API_KEY}"
 ZONE_ID="33aef43c9924965d6d3bc43fa99506a4"
 
 curl -s -X GET https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?per_page=500 \
